14274176699a8b4608961fac19b9f0093d0ab453
Will Bickford <wbic16@gmail.com>
2024-12-02 16:58:41 -0600

    Second Star - Day 1

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn day1(input: String) -> i32 {
     left.push(pair[0].parse::<i32>().expect("error 1"));
     right.push(pair[1].parse::<i32>().expect("error 2"));
   }
-  println!("Loaded Left: {}, Right: {} items.", left.len(), right.len());
+  // println!("Loaded Left: {}, Right: {} items.", left.len(), right.len());
 
   left.sort();
   right.sort();
@@ -31,12 +31,37 @@ fn day1(input: String) -> i32 {
   return total;
 }
 
+fn day1_part2(input: String) -> usize {
+  let mut left: Vec<i32> = Vec::new();
+  let mut right: Vec<i32> = Vec::new();
+  
+  let lines = input.split('\n').into_iter();
+  for line in lines {
+    let pair: Vec<&str> = line.split_whitespace().collect();
+    left.push(pair[0].parse::<i32>().expect("error 1"));
+    right.push(pair[1].parse::<i32>().expect("error 2"));
+  }
+  // println!("Loaded Left: {}, Right: {} items.", left.len(), right.len());
+
+  let mut similarity: usize = 0;
+  //let mut i: usize = 0;
+  for number in left {
+    let count = right.iter().filter(|s| **s == number).count();
+    // un-comment to audit the process
+    //if i < 10 && count > 0 { println!("{}: {} vs {}: {}", i, number, count, similarity); i += 1; }
+    similarity += (number as usize) * count;
+  }
+
+  return similarity;
+}
+
 fn main() {
   println!("Advent of Code 2024");
 
   let filename = "problems.phext";
   let problems = fs::read_to_string(filename).expect("Unable to locate problem set.");
   let scroll1 = phext::fetch(problems.as_str(), phext::to_coordinate("1.1.1/1.1.1/1.1.1"));    
-  let result1 = day1(scroll1);
-  println!("Day 1: {}", result1);
+  let result1_1 = day1(scroll1.clone());
+  let result1_2 = day1_part2(scroll1);
+  println!("Day 1: {} + {}", result1_1, result1_2);
 }
