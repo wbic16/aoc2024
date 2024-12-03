a6f13ca3aa34bcc7b7eae422a8baa17a500b8043
Will Bickford <wbic16@gmail.com>
2024-12-03 00:39:08 -0600

    Day 2 Star 2 Progress

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ https://adventofcode.com/2024/day/1
 
 The first day provides us with a text list of 2,000 numbers arranged into 1,000 pairs of lines.
 
-Gold Star 1: We sort the lists and calculate the distance between the elements: 765748.
-Gold Star 2: Next up, we need to calculate a similarity score: 27732508.
+Gold Star 1: We sort the lists and calculate the distance between the elements: 765,748.
+Gold Star 2: Next up, we need to calculate a similarity score: 27,732,508.
 
 In order to solve today's puzzle, you need to make use of conversions from text. I earned two gold stars with 68 lines of Rust.
 
@@ -27,5 +27,10 @@ https://adventofcode.com/2024/day/2
 The second day asks us to start reviewing reactor reports for safe/unsafe operation.
 
 In order to solve today's puzzle, you need to continue learning how to parse/split text into integers.
+The second gold star is a bit of an exercise in frustration with edge cases.
+
 
 Gold Star 1: In our data set, only 660 of the 1,000 reports are marked safe!
+Gold Star 2: Enter the Problem Dampener - currently unsolved (687-701)
+
+# Day 3: Pending
--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn day2(input: String) -> usize {
     let mut is_increasing = false;
     let mut no_change_observed = false;
     let mut big_delta_observed = false;
-    let mut started = false;
+    let mut started = false;    
     for element in row.clone() {
       if started { prior = value; }
       value = element.parse::<i32>().expect("error 1");
@@ -105,6 +105,125 @@ fn day2(input: String) -> usize {
   return result;
 }
 
+#[derive(Default)]
+pub struct Day2Node {
+  pub value: i32,
+  pub enabled: bool
+}
+impl Day2Node {
+  pub fn new() -> Day2Node {
+    Day2Node { value: 0, enabled: false }
+  }
+}
+
+fn day2_tokenize(values: &str) -> Vec<Day2Node> {
+  let mut tokens: Vec<Day2Node> = Default::default();
+
+  let row: Vec<&str> = values.split_whitespace().collect();
+  for item in row {
+    let value = item.parse::<i32>().expect("error 1");
+    let mut next = Day2Node::new();
+    next.value = value;
+    next.enabled = true;
+    tokens.push(next);
+  }
+
+  return tokens;
+}
+
+fn day2_validate(line: &str, report: &Vec<Day2Node>) -> bool {
+  let mut prior;
+  let mut started = false;
+  let mut value = 0;
+  let mut increasing = 0;
+  let mut result = false;
+  let mut index = 0;
+  for node in report {
+    index += 1;
+    if !node.enabled {
+      println!("{}: Skipping index {}", line, index);
+      continue;
+    }
+
+    if !started {
+      started = true;
+      value = node.value;
+      continue;
+    }
+    
+    prior = value;
+    value = node.value;
+    if increasing == 0 {
+      if value > prior { increasing = 1;  }
+      if value < prior { increasing = -1; }
+    }
+
+    let delta = (value - prior).abs();
+    if delta > 3 {
+      println!("{}: Step Failure at index {} ({}-{}) due to delta > 3.", line, index, value, prior);
+      return false;
+    }
+    if delta == 0 {
+      println!("{}: Step Failure at index {} ({}-{}) due to delta == 0.", line, index, value, prior);
+      return false;
+    }
+    if delta < 0 && increasing == 1  {
+      println!("{}: Step Failure at index {} ({}-{}) due to -/+ mismatch.", line, index, value, prior);
+      return false;
+    }
+    if delta > 0 && increasing == -1 {
+      println!("{}: Step Failure at index {} ({}-{}) due to +/- mismatch.", line, index, value, prior);
+      return false;
+    }
+
+    result = true;
+  }
+
+  return result;
+}
+
+fn day2_part2(input: String) -> usize {
+    let lines = input.split('\n').into_iter();
+    let mut result = 0;
+    let mut blip: i32 = -1;
+  
+    for line in lines {
+      let mut items = day2_tokenize(line);
+
+      let start: i32 = 0;
+      let end = items.len() as i32;
+
+      let mut passed = day2_validate(line, &items);
+      if passed {
+        result += 1;
+      } else {
+        blip = start as i32;
+        while (passed == false) && (blip < end) {
+          let mut ri: usize = 0;
+          while ri < end as usize {
+            items[ri].enabled = ri != (blip as usize);
+            ri += 1;
+          }
+          passed = day2_validate(line, &items);
+          if passed {
+            result += 1;
+          }
+          blip += 1;
+        }
+      }
+
+      if passed == false {
+        println!("{}: Failed\n\n", line);
+      } else {
+        let mut blipped = format!("");
+        if blip >= 0 { blipped = format!(" ({})", blip); }
+        println!("{}: Passed{}\n", line, blipped);
+      }
+    }
+  
+    return result;
+  }
+
 fn main() {
   println!("Advent of Code 2024");
 
@@ -117,5 +236,6 @@ fn main() {
 
   let scroll2 = phext::fetch(problems.as_str(), phext::to_coordinate("1.1.1/1.1.1/1.1.2"));
   let result2_1 = day2(scroll2.clone());
-  println!("Day 2: {} + {}", result2_1, 0);
+  let result2_2 = day2_part2(scroll2); // 679,687 too low, 701 too high
+  println!("Day 2: {} + {}", result2_1, result2_2);
 }
