0bbe23d3f9dfba89625b2250db0fb226864b4bb0
Will Bickford <wbic16@gmail.com>
2024-12-05 23:53:51 -0600

    Noodled a bit on Day 6 before bed

--- src/day6.rs
+++ src/day6.rs
@@ -5,3 +5,66 @@ pub fn main(_input: String) -> usize {
 pub fn part2(_input: String) -> usize {
   return 0;
 }
+
+fn sample_input() -> String {
+  return "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...".to_string();
+}
+
+fn sample_output() -> String {
+  return "....#.....
+....XXXXX#
+....X...X.
+..#.X...X.
+..XXXXX#X.
+..X.X.X.X.
+.#XXXXXXX.
+.XXXXXXX#.
+#XXXXXXX..
+......#X..".to_string();
+}
+
+// ^ - up
+// > - right
+// V - down
+// < - left
+
+struct Day6Coord {
+  x: isize,
+  y: isize
+}
+
+fn offset2pos(rows: usize, offset: usize) -> Day6Coord {
+  return Day6Coord{ x: (offset % rows) as isize, y: (offset/rows) as isize };
+}
+fn pos2offset(rows: usize, coord: &Day6Coord) -> usize {
+  return (coord.x + (rows as isize) * coord.y) as usize;
+}
+
+fn day6_path_transform(input: String) -> String {
+  let rows = input.split('\n').count();
+  let serialized = input.replace("\n", "");
+  // 0-9
+  let left = serialized.find('<');
+  if left.is_some() {
+    let mut lc = offset2pos(rows, left.expect("unexpected"));      
+  }
+  let up = serialized.find('^');
+  let right = serialized.find('>');
+  let down = serialized.find('V');
+
+  return "".to_string();
+}
+
+#[test]
+fn test_visit_locations() {
+  
+}
